bc28643b29b466c4c49ca0a3e239d891e746a1f3
Taybul1 <139383900+Taybul1@users.noreply.github.com>
2024-05-04 16:44:42 +0600

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,40 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;31m                                           
+   .d88b.  d8888b. d8888b.  .d88b.  
+  .8P  Y8. 88  `8D 88  `8D .8P  Y8. 
+  88    88 88oodD' 88oodD' 88    88 
+  88    88 88~~~   88~~~   88    88 
+  `8b  d8' 88      88      `8b  d8' 
+   `Y88P'  88      88       `Y88P'  
+   
+     \033[1;96m_____________________________________                           
+                 \033[1;31m TAYBUL ISLAM  
+     \033[1;96m_____________________________________
+
+ \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m)"""
+
+#INTIALIZING
+#ALL SETUP
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+echo " " 
+sleep 2
+cp 1 .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+printf "echo -e '\e[0m\e[1;32m'_____________________________________________________________" >> .bashrc
+mv .bashrc ~
+#INTIALIZING
+#ALL SETUP 
+#PLEASE WAIT FOR SETUPING
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+echo " " 
+sleep 2
+#INTIALIZING
+#ALL SETUP DONE
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+echo " " 
+sleep 1
+#INTIALIZING FINISHED 
+#ALL SETUP DONE RESTART YOUR TERMUX 
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
+echo " " 
